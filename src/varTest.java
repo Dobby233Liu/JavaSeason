9f48c7bc1a74bd723391986feba097513e0b16b7
Liu-Wen-Yuan <15816141883@163.com>
2017-05-30 00:17:44 -0500

    Create varTest.java

---
+++ src/varTest.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class varTest
+{
+	public static void main(String[] args)
+	{
+    int bakamode = 1;
+	if(bakamode == 1)
+	System.out.println("hey, baka! ");
+	else
+	{
+		System.out.println("Get a error: Not a Real data");
+		
+		
+		
+		
+    } 
+	}
+}
